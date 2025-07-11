81d27a088cff9492eb4f5571999654678905ab23
GiulioM90 <giuliomollo90@gmail.com>
2025-07-11 07:42:05 +0200

    chore: added the type return from function

--- src/lib/loadFolder.ts
+++ src/lib/loadFolder.ts
@@ -1,8 +1,9 @@
 import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
 import { TextLoader } from "langchain/document_loaders/fs/text";
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { Document } from "@langchain/core/documents";
 
-export const loadFolder = async (path: string) => {
+export const loadFolder = async (path: string): Promise<Document[]> => {
     const loader = new DirectoryLoader(path, {
     '.pdf': (path: string) => new PDFLoader(path, {
       splitPages: false
